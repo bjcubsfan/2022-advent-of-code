abb854f24d9e84aa3f9efcc610389424c175f761
B. J. Potter <bj.ctr.potter@faa.gov>
2022-11-30 23:55:34 -0600

    Solve day 1 part 2

--- 01/solution.py
+++ 01/solution.py
@@ -14,6 +14,8 @@ def elf_totals(input_data):
             curr_elf = 0
         else:
             curr_elf += int(line)
+    # Do not forget the last elf
+    totals.append(curr_elf)
     return totals
 
 def part_1(input_data):
@@ -21,11 +23,8 @@ def part_1(input_data):
     return max(totals)
 
 def part_2(input_data):
-    input_data = input_data.strip()
-    answer = None
-    for line in input_data.split("\n"):
-        line = line.strip()
-    return answer
+    totals = elf_totals(input_data)
+    return sum(sorted(totals, reverse=True)[0:3])
 
 def main():
     with open("input.txt") as input_file:
